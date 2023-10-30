4d02fe3b44f51953c06d5f1764f5a57790388933
Boris Vesely <vesely.svk@gmail.com>
2023-10-31 00:53:49 +0100

    added get

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type FPGA struct {
+	ID        string `json:"id"`
+	IP        string `json:"ip"`
+	State     string `json:"state"`
+	Timestamp string `json:"timestamp"`
+	Action    string `json:"aciton"`
+	IPTunnel  string `json:"iptunnel"`
+}
+
+var FPGAs = []FPGA{
+	{ID: "1", IP: "10.0.2.4", State: "DEFAULT", Timestamp: "", Action: "", IPTunnel: ""},
+}
+
+func getFPGAs(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, FPGAs)
+}
+
 func main() {
-	fmt.Println("Hello tesitng this")
+	router := gin.Default()
+	router.GET("/FPGAs", getFPGAs)
+
+	router.Run("localhost:8080")
 }
